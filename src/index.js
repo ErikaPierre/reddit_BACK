2c207eb801e76d542adf2a7efd092940bb4d4e31
Erika <pierreerika@MBP-de-Erika.lan>
2024-02-01 15:41:47 +0100

    Configuration de la BDD

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log(`📁DATABASE CONNECTED🐳`);
+}
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Welcom on API Reddit");
+});
+
+app.listen(port, () =>
+  console.log(`Server is listening on port : http://localhost:${port}`)
+);
